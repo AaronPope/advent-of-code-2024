db62d84d0ee8aaee7cd8021522c349c7db503bd4
Aaron Pope <aaron.m.pope.ctr@mail.mil>
2024-12-11 17:21:34 -0500

    completed day 3, part 2

--- day3/day3.go
+++ day3/day3.go
@@ -19,26 +19,61 @@ func main() {
 	// 	fmt.Println(string(v))
 	// }
 
+	part1 := Part1() // 190604937
+	part2 := Part2() // 82857512
+	fmt.Printf("Part 1: %v\nPart 2: %v\n", part1, part2)
+}
+
+func Part1() int {
 	file, err := os.Open("inputs/day3.txt")
 
 	if err != nil {
 		fmt.Errorf("Failed to load input")
 	}
 	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	input := ""
+	for scanner.Scan() {
+		input += scanner.Text()
+	}
 
-	part1 := Part1(file)
-	part2 := Part2()
-	fmt.Printf("Part 1: %v\nPart 2: %v\n", part1, part2)
+	product := 0
+
+	muls := re.FindAllString(input, -1)
+
+	for _, v := range muls {
+		nums := digitsRE.FindAllString(v, -1)
+
+		n1, _ := strconv.Atoi(nums[0])
+		n2, _ := strconv.Atoi(nums[1])
+
+		product += n1 * n2
+	}
+
+	return product
 }
 
-func Part1(input *os.File) int {
-	scanner := bufio.NewScanner(input)
-	product := 0
+func Part2() int {
+	// Had to add `do()` to the beginning and `don't()` to the end of the file
+	//	since muls are active at the beginning and a `do()` is near the end but not closed by a `don't()`
+	file, err := os.Open("inputs/day3-part2.txt")
+
+	if err != nil {
+		fmt.Errorf("Failed to load input")
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	input := ""
 	for scanner.Scan() {
-		chunk := scanner.Text()
+		input += scanner.Text()
+	}
 
-		muls := re.FindAllString(chunk, -1)
+	activeMulsRE := regexp.MustCompile(`do\(\)(.*?)don't\(\)`)
+	activeSections := activeMulsRE.FindAllString(input, -1)
 
+	product := 0
+	for _, v := range activeSections {
+		muls := re.FindAllString(v, -1)
 		for _, v := range muls {
 			nums := digitsRE.FindAllString(v, -1)
 
@@ -48,9 +83,6 @@ func Part1(input *os.File) int {
 			product += n1 * n2
 		}
 	}
-	return product
-}
 
-func Part2() int {
-	return 0
+	return product
 }
