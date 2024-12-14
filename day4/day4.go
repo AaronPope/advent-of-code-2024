f70114d3c7c1ae65cfaa36942f663f34e346ef51
Aaron Pope <aaron.m.pope.ctr@mail.mil>
2024-12-14 00:33:57 -0500

    bootstrapped day 4

---
+++ day4/day4.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	part1 := Part1()
+	part2 := Part2()
+
+	fmt.Printf("Part 1: %v\nPart 2: %v\n", part1, part2)
+}
+
+func Part1() int {
+	return 0
+}
+
+func Part2() int {
+	return 0
+}
